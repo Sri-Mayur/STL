54d69cd5b772e334e1a0421983801caf95c25853
Sri-Mayur <mayursrivastava877@gmail.com>
2023-11-20 19:40:42 +0530

    Added some description

--- Vector.cpp
+++ Vector.cpp
@@ -3,17 +3,25 @@
 using namespace std;
 
 int main(){
-    vector<int> v;
-
+    
+    vector<int> v; // This is how you'll initialize or declare the vector.
+     
+    // push_back() is use to push elements into the vector. 
     v.push_back(4);
     v.push_back(5);
     v.push_back(7);
+    v.push_back(8);
+    v.push_back(6);
+    // Vector will look like this: 4 5 7 8 6
 
+    // pop_back() is use to delete the element at last index.
+    v.pop_back(); 
+
+
+    // v.size() provides the size of the vector or the numbers of elements present in vector.
     for(int i=0; i<v.size(); i++){
-        cout<<v[i]<<" ";
+        cout<<v[i]<<" "; //It will print 4 5 7 8
     }
-
-
 
     return 0;
 }
